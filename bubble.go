3a534c03a1ec15b5c82a27a07d036822b518056b
jshields <shields.h.josh@gmail.com>
2020-01-12 12:37:14 -0600

    bubble

---
+++ bubble.go
@@ -0,0 +1,16 @@
+package sort
+
+func bubble(arr []int) []int {
+	n = len(arr)
+	for i := 0; i < n; i++ {
+		// -i because i-th iteration has bubbled up the i-th largest number
+		// -1 because we look ahead by 1 item
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				// no need for tmp variable when using multiple assignment
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}
---
+++ bubble_test.go
@@ -0,0 +1,39 @@
+package sort
+
+import "testing"
+
+func slicesEqual(arr1, arr2) bool {
+	if len(arr1) != len(arr2) {
+		return false
+	}
+	for ind, val := range arr1 {
+		if val != arr2[ind] {
+			return false
+		}
+	}
+	return true
+}
+
+func Test_bubble(t *testing.T) {
+	testCases := map[string]struct{
+		input []int
+		expected []int
+	}{
+		"empty": {input: []int{}, expected: []int{}},
+		"zero": {input: []int{0}, expected: []int{0}},
+		"3-2-1": {input: []int{3, 2, 1},expected: []int{1, 2, 3}},
+		"with duplicates": {input: []int{2, 1, 8, 5, 5, 2, 3, 2, 3, 7}, expected: []int{1, 2, 2, 2, 3, 3, 5, 5, 7, 8}},
+		"some big numbers": {input: []int{500, 200, 1000, 2, 70, 99999}, expected: []int{2, 70, 200, 500, 1000, 99999}},
+		"negative numbers only": {[]int{-5, -2, -6, -1}: expected: []int{-6, -5, -2, -1}},
+		"negative numbers mixed": {input: []int{1, -1, 2, -3, 5, 9, -9}, expected: []int{-9, -3, -1, 1, 2, 5, 9}},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func (t *testing.T) {
+			actual := bubble(tc.input)
+			if !slicesEqual(arr1, arr2) {
+				// t.Fatalf
+			}
+		})
+	}
+}
